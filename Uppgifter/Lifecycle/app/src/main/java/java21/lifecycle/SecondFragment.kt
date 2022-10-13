001cbe0c56049473e56604fd584ccd530e88c3b7
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2022-10-13 14:06:43 +0200

    Added errortext

--- Uppgifter/Lifecycle/app/src/main/java/java21/lifecycle/SecondFragment.kt
+++ Uppgifter/Lifecycle/app/src/main/java/java21/lifecycle/SecondFragment.kt
@@ -1,5 +1,6 @@
 package java21.lifecycle
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,10 +10,14 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import android.widget.SpinnerAdapter
+import android.widget.TextClock
+import android.widget.TextView
 import android.widget.Toast
 import java.text.FieldPosition
+import java.util.jar.Attributes.Name
 import kotlin.math.log
 
 class SecondFragment : Fragment() {
@@ -21,15 +26,26 @@ class SecondFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_second, container, false)
 
+        val nameText: TextView = view.findViewById(R.id.formName)
+        val bdayText: TextView = view.findViewById(R.id.formBday)
+        val eIDText: TextView = view.findViewById(R.id.formID)
+
+
+        val saveBtn: Button = view.findViewById(R.id.formButton)
+        val errorTxt: TextView = view.findViewById(R.id.formErrorText)
+
         //Accessing the list of countries
         val countryList: Array<String> = resources.getStringArray(R.array.country_list_form)
         Log.i("test", countryList[1])
+
+        var selectedCountry: String = ""
 
         //Get access to spinner (for dropdown menu)
         val spinner: Spinner = view.findViewById(R.id.spinner)
@@ -43,9 +59,44 @@ class SecondFragment : Fragment() {
                 override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                     val item = parent?.getItemAtPosition(position).toString()
                     Log.i("spinnerTest", (parent?.getItemAtPosition(position).toString() + " SELECTED"))
+                    selectedCountry = (parent?.getItemAtPosition(position).toString())
                 }
+        }
+        //todo: get boolean from checkbox
+
+        saveBtn.setOnClickListener{
+            var isValid: Boolean = checkFields(nameText.text.toString(), bdayText.text.toString(),
+            eIDText.text.toString(), selectedCountry)
+            if (!isValid) {
+                errorTxt.text = "Please set input in all fields!"
+                Log.i("saveButton", "Rejected field input")
+            }
+            else {
+                errorTxt.text = ""
+                Log.i("saveButton", "Accepted field input")
+            }
         }
 
         return view
+    }
+
+    private fun checkFields(name: String, birthDate: String, employeeID: String, officeLocation: String) :Boolean {
+        var areFieldsFilled: Boolean = true
+
+        //Checks for invalid (or empty) inputs
+        if (name == "Name" || name.isEmpty() || birthDate == "Date of Birth (YYYY/MM/DD" || birthDate.isEmpty()
+            || employeeID == "Employee ID" || employeeID.isEmpty() || officeLocation.isBlank()){
+            areFieldsFilled = false
+        }
+
+        //If no errors are found, saves the input data
+        if (areFieldsFilled){
+            MainActivity.name = name
+            MainActivity.birthdate = birthDate
+            MainActivity.employee_id = Integer.parseInt(employeeID)
+            MainActivity.officeLocation = officeLocation
+        }
+
+        return areFieldsFilled
     }
 }
