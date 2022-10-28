70c63dc57411ca600129875dedb5466b1f64a62b
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2022-10-28 15:52:16 +0200

    Microphone implemented

--- Uppgifter/VoiceToNewsApp/app/src/main/java/java21/voicetonews/SearchFragment.kt
+++ Uppgifter/VoiceToNewsApp/app/src/main/java/java21/voicetonews/SearchFragment.kt
@@ -1,26 +1,56 @@
 package java21.voicetonews
 
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class SearchFragment : Fragment() {
+
+    // on below line we are creating a constant value
+    private val REQUEST_CODE_SPEECH_INPUT = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
 
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_search, container, false)
 
-        
+        val recBtn: Button = view.findViewById(R.id.searchRecordButton)
+        val recOutputText: TextView = view.findViewById(R.id.searchOutputText)
 
+        recBtn.setOnClickListener {
+            // calling speech recognizer intent.
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+
+            // passing language model and model free form in our intent
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+
+            // passing our language as a default language.
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+
+            // specifying a prompt message as speak to text on below line.
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text")
+
+            // calling a start activity for result method and passing our result code.
+            try {
+                startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
+            } catch (e: Exception) {
+                Toast.makeText(context, e.message.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
         return view
     }
 
