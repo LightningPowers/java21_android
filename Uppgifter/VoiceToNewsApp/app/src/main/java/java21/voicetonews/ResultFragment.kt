224ba9673cd987681d8deb2e44a62cef87a524a6
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2022-10-31 13:18:27 +0100

    Add mic-to-text output

--- Uppgifter/VoiceToNewsApp/app/src/main/java/java21/voicetonews/ResultFragment.kt
+++ Uppgifter/VoiceToNewsApp/app/src/main/java/java21/voicetonews/ResultFragment.kt
@@ -15,7 +15,7 @@ class ResultFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_result, container, false)
--- Uppgifter/VoiceToNewsApp/app/src/main/java/java21/voicetonews/SearchFragment.kt
+++ Uppgifter/VoiceToNewsApp/app/src/main/java/java21/voicetonews/SearchFragment.kt
@@ -1,5 +1,6 @@
 package java21.voicetonews
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognizerIntent
@@ -14,44 +15,64 @@ import java.util.*
 
 class SearchFragment : Fragment() {
 
-    // on below line we are creating a constant value
-    private val REQUEST_CODE_SPEECH_INPUT = 1
+    // Creating a constant value
+    private val resultInputCode = 1
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    lateinit var recBtn: Button
+    lateinit var recOutputText: TextView
+    lateinit var errorText: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_search, container, false)
 
-        val recBtn: Button = view.findViewById(R.id.searchRecordButton)
-        val recOutputText: TextView = view.findViewById(R.id.searchOutputText)
+        recBtn = view.findViewById(R.id.searchRecordButton)
+        recOutputText = view.findViewById(R.id.searchOutputText)
+        errorText= view.findViewById(R.id.searchStatusText)
 
         recBtn.setOnClickListener {
-            // calling speech recognizer intent.
+            // Declaring speech recognizer intent.
             val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
 
-            // passing language model and model free form in our intent
+            // Setting language model and free form model as intent
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
 
-            // passing our language as a default language.
+            // Setting system default language as target language
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
 
-            // specifying a prompt message as speak to text on below line.
+            // Specifying message that shows in window
             intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text")
 
-            // calling a start activity for result method and passing our result code.
+            // Calling start activity for result method, passing the resultInputCode
             try {
-                startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
+                startActivityForResult(intent, resultInputCode)
             } catch (e: Exception) {
                 Toast.makeText(context, e.message.toString(), Toast.LENGTH_SHORT).show()
+                errorText.text = e.message.toString()
             }
         }
         return view
     }
 
+    // Retrieving data from recording
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Checking request code with the resultInputCode
+        if (requestCode == resultInputCode) {
+            // Checking if result code is ok
+            if (resultCode == RESULT_OK && data != null) {
+
+                // Extracting the data from result array list
+                val res: ArrayList<String> =
+                    data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS) as ArrayList<String>
+
+                // Displaying output as text in text output box
+                recOutputText.text = Objects.requireNonNull(res)[0]
+            }
+        }
+    }
 }
