2dc7706be82140679088877f9458375f6c4f3c82
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2022-11-02 17:36:19 +0100

    More cleanup, changed placeholder img

--- Uppgifter/VoiceToNewsApp/app/src/main/java/java21/voicetonews/ApiHelper.kt
+++ Uppgifter/VoiceToNewsApp/app/src/main/java/java21/voicetonews/ApiHelper.kt
@@ -18,13 +18,6 @@ class ApiHelper {
         fun jsonToArticles(response: JSONObject) : ArrayList<ArticleBean> {
             val beanList: ArrayList<ArticleBean> = arrayListOf() //Todo: put in data here
 
-            //Template bean
-            val baseBean: ArticleBean = ArticleBean(
-                "Man Can’t Remember What He Ran Into Burning Building For",
-                "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
-                "Fauxnews",
-                "2022-11-01 06:14:21")
-
             val articleList: JSONArray = response.getJSONArray("results")
             for (i in 0 until articleList.length()) {
                 //Set value to placeholder (template) bean
@@ -33,17 +26,19 @@ class ApiHelper {
                 //Get variables from data
                 val source: String = jsonArticle.getString("source_id")
                 val title: String = jsonArticle.getString("title")
+                val imageUrl: String
                 val date: String = jsonArticle.getString("pubDate")
 
                 // Prevent against URLs that are null
-                /*if (jsonArticle.optString("image_url", null).isNullOrBlank()){
-                    beanArticle.imageURL = baseBean.imageURL
-                    Log.d("testApi", "ImageUrl: " + beanArticle.imageURL)
+                if (jsonArticle.optString("image_url") == "null") {
+                    imageUrl = "https://www.mirage-systems.de/fileadmin/user_upload/placeholder-news.jpg"
+                    Log.d("testApi", "true - null")
                 }
                 else {
-                    beanArticle.imageURL = jsonArticle.getString("image_url")
-                    Log.d("testApi", "ImageUrl: " + beanArticle.imageURL)
-                }*/
+                    imageUrl = jsonArticle.getString("image_url")
+                    Log.d("testApi", "false - not null")
+                    Log.d("image", imageUrl)
+                }
 
                 //Format date
                 val firstApiFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
@@ -59,7 +54,7 @@ class ApiHelper {
                 Log.d("parseTesting", finalDate)
 
                 //Add bean to beanList
-                beanList.add(ArticleBean(title,baseBean.imageURL,source.replaceFirstChar{it.uppercase()},finalDate))
+                beanList.add(ArticleBean(title,imageUrl,source.replaceFirstChar{it.uppercase()},finalDate))
             }
             return beanList
         }
